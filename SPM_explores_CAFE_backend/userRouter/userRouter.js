ed04f603ce90508323ec43471e1bb33d74962c7a
prabodhalahiru <prabodhalahiru119@gmail.com>
2021-09-05 22:49:26 +0530

    added some comments

--- SPM_explores_CAFE_backend/userRouter/userRouter.js
+++ SPM_explores_CAFE_backend/userRouter/userRouter.js
@@ -4,8 +4,11 @@ const auth = require('../middleware/auth')
 
 router.post('/register', userCtrl.register) 
 router.post('/login', userCtrl.login) 
+
+//logout router 
 router.get('/logout', userCtrl.logout) 
 router.post('/refresh_token', userCtrl.refreshToken) 
+//this is used to get user informations
 router.get('/infor',auth, userCtrl.getUser)
 
 module.exports = router
