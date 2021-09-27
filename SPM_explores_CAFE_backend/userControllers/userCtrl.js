453628ec8684dfd381cb362574acca4f56306bfc
prabodhalahiru <prabodhalahiru119@gmail.com>
2021-09-27 19:12:44 +0530

    Order management

--- SPM_explores_CAFE_backend/userControllers/userCtrl.js
+++ SPM_explores_CAFE_backend/userControllers/userCtrl.js
@@ -8,7 +8,7 @@ const jwt = require('jsonwebtoken');
 
 
 
-const userC = {
+const userCtrl = {
 
     register: async(req, res) => {
         try{
@@ -131,6 +131,18 @@ const userC = {
         }catch(err){
            return res.status(500).json({msg:err.message}) 
         }
+    },
+    addCart: async (req,res) =>{
+        try{
+            const user = await Users.findById(req.user.id)
+            if(!user) return res.status(400).json({msg:"User does not exist"})
+
+            await Users.findOneAndUpdate({_id: req.user.id},{
+                cart: req.body.cart
+            })
+        }catch(err){
+            return res.status(500).json({msg:err.message}) 
+        }
     }
 }
 
@@ -143,4 +155,4 @@ const createRefreshToken = (user) => {
     return jwt.sign(user, process.env.REFRESH_TOKEN_SECRET, {expiresIn:'7d'})
 }
 
-module.exports = userC
+module.exports = userCtrl
